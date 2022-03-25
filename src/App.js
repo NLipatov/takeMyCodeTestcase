24a5e7be792af676a698f4a399472414ea7b24bd
LipatovNN <lipatovnn@yahoo.com>
2022-03-25 18:38:29 +0300

    layout improvements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ItemView from './components/ItemView';
 
+import './App.css';
+
 function App() {
   return (
-    <ItemView/>
+    // <ItemView/>
+    <div className="main">
+      <div className="header">
+
+      </div>
+      <div className="filterMenu">
+
+      </div>
+      <div className="items">
+
+      </div>
+    </div>
   );
 }
 
